481bbae9b29313603d9d76bc74021dc78cb96ff6
Gabriele <g.nocchi@hotmail.com>
2019-10-31 19:40:48 +0000

    Updated code to plot any PC. Updated bnw section.

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,4 @@
-plotPlinkPca <- function(file,file2, x = 1, y = 2){
+plotPlinkPca <- function(file, file2, x = 1, y = 2){
 	
 	library(ggplot2)
 	library(grid)
@@ -11,10 +11,9 @@ plotPlinkPca <- function(file,file2, x = 1, y = 2){
 		mydata <- data.frame(read.table(file))
 		rownames(mydata) <- mydata[,2]
 		mydata <- mydata[,3:ncol(mydata)]
-		colnames(mydata) <- c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10")
 		
-		p <- ggplot(mydata,aes(file=PC1,file2=PC2))
-		p <- p+geom_point() + ggtitle("PCA")
+		p <- ggplot(mydata,aes(x=mydata[,x],y=mydata[,y]))
+		p <- p + geom_point() + ggtitle("PCA") + xlab(paste("PC",x, sep ="")) + ylab(paste("PC",y, sep = ""))
 
 		return(p)
 		
@@ -23,14 +22,14 @@ plotPlinkPca <- function(file,file2, x = 1, y = 2){
         
 		mydata <- data.frame(read.table(file))
 		group <- read.table(file2, stringsAsFactors = FALSE)
+		colnames(group) <- c("group")
 	
 		rownames(mydata) <- mydata[,2]
 		mydata <- mydata[,3:ncol(mydata)]
 		mydata <- cbind(mydata,group)
-		colnames(mydata) <- c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10", "Population")
 
-		p <- ggplot(mydata,aes(x=mydata[,x],y=mydata[,y],color=Population))
-		p <- p+geom_point() + scale_color_brewer(palette="Set1") + ggtitle("PCA") + xlab(paste("PC",x, sep ="")) + ylab(paste("PC",y, sep = ""))
+		p <- ggplot(mydata,aes(x=mydata[,x],y=mydata[,y],color=group))
+		p <- p + geom_point() + scale_color_brewer(palette="Set1") + ggtitle("PCA") + xlab(paste("PC",x, sep ="")) + ylab(paste("PC",y, sep = ""))
 
 		return(p)
 	
