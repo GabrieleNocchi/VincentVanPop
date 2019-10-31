dd110846a9177ee9927df67c8b991a97d125a4e6
Gabriele <g.nocchi@hotmail.com>
2019-10-31 12:51:04 +0000

    Added more options to plotPlinkPca

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,4 @@
-plotPlinkPca <- function(x,y){
+plotPlinkPca <- function(file,file2, x = 1, y = 2){
 	
 	library(ggplot2)
 	library(grid)
@@ -6,14 +6,14 @@ plotPlinkPca <- function(x,y){
 
 
 
-	if(missing(y)) {
+	if(missing(file2)) {
         
-		mydata <- data.frame(read.table(x))
+		mydata <- data.frame(read.table(file))
 		rownames(mydata) <- mydata[,2]
 		mydata <- mydata[,3:ncol(mydata)]
 		colnames(mydata) <- c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10")
 		
-		p <- ggplot(mydata,aes(x=PC1,y=PC2))
+		p <- ggplot(mydata,aes(file=PC1,file2=PC2))
 		p <- p+geom_point() + ggtitle("PCA")
 
 		return(p)
@@ -21,16 +21,16 @@ plotPlinkPca <- function(x,y){
 		
     } else {
         
-		mydata <- data.frame(read.table(x))
-		group <- read.table(y, stringsAsFactors = FALSE)
+		mydata <- data.frame(read.table(file))
+		group <- read.table(file2, stringsAsFactors = FALSE)
 	
 		rownames(mydata) <- mydata[,2]
 		mydata <- mydata[,3:ncol(mydata)]
 		mydata <- cbind(mydata,group)
 		colnames(mydata) <- c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10", "Population")
 
-		p <- ggplot(mydata,aes(x=PC1,y=PC2,color=Population))
-		p <- p+geom_point() + scale_color_brewer(palette="Set1") + ggtitle("PCA")
+		p <- ggplot(mydata,aes(x=mydata[,x],y=mydata[,y],color=Population))
+		p <- p+geom_point() + scale_color_brewer(palette="Set1") + ggtitle("PCA") + xlab(paste("PC",x, sep ="")) + ylab(paste("PC",y, sep = ""))
 
 		return(p)
 	
